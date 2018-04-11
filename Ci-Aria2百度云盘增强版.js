32d3812807618976f863c7782993ba2820fb92fc
Ci <815362636@qq.com>
2018-04-11 16:02:15 +0800

    Update Ci-Aria2百度云盘增强版.js

--- Ci-Aria2百度云盘增强版.js
+++ Ci-Aria2百度云盘增强版.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ci-Aria2百度云盘增强版
 // @namespace    https://github.com/CiChui/Ci-Aria2BDY/
-// @version      0.1.4
+// @version      1.0.0
 // @description  百度网盘文件直链提取, 支持一键发送至Aria2进行下载
 // @author       CiChui
 // @license      MIT
